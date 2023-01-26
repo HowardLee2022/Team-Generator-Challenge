951b68922962b74a87e60e70760ab2ab690ee2ff
howard <howardnlee94@gmail.com>
2023-01-26 15:35:15 -0800

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
-const inquirer = require("inquirer")
-const Manager = require("./lib/Manager")
-const Intern = require("./lib/Intern")
-const Engineer =require("./lib/Engineer")
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer =require("./lib/Engineer");
+const fs = require("fs");
+const generateHtml = require("./util/generateHtml");
 
 
 const employed = [];
@@ -10,96 +12,105 @@ const todo = () =>{
     inquirer.prompt({
      
         type:"list",
-        name:"choose"
-        message:"What do you want to do?"
+        name:"choose",
+        message:"What do you want to do?",
         choices:["Add intern","Add Engineer","Quit"]
     }).then(ans=>{
         if(ans.choose ==="Add intern"){
             addIntern();
-            return todo();
         }else if(ans.choose =="add Engineer"){
-            addEngineer
+            addEngineer();
+        }else{
+            fs.writeFile("index.html", generateHtml(employed), (err)=> err ? console.log(err):console.log("html file created"));
         }
     })
 }
 
 const addIntern = () => {
-    inquirer.prompt({
-       {type:"input"
-        name:"name"
+    inquirer.prompt([
+    {   
+        type:"input",
+        name:"name",
         message:"What intern name?"
     },
     {
-        type:"input"
-        name:"id"
+        type:"input",
+        name:"id",
         message:"what is intern id?"
     },
     {
-        type:"input"
-        name:"email"
+        type:"input",
+        name:"email",
         message:"what is intern email?"
     },
     {
-        type:"input"
-        name:"school"
+        type:"input",
+        name:"school",
         message:"what is intern school?"
     }
-    }).then(ans => {
+    ]).then(ans => {
         const newIntern = new Intern(ans.name, ans.id, ans.email, ans.school)
         employed.push(newIntern);
     })
+    todo();
 }
 
 const addManager = () => {
-    inquirer.prompt({
-       {type:"input"
-        name:"name"
-        message:"What manager name?"
+    inquirer.prompt([
+    {   
+        type:"input",
+        name:"name",
+        message:"What the manager name?"
     },
     {
-        type:"input"
-        name:"id"
-        message:"what is manager id?"
+        type:"input",
+        name:"id",
+        message:"what is the manager id?"
     },
     {
-        type:"input"
-        name:"email"
-        message:"what is manager email?"
+        type:"input",
+        name:"email",
+        message:"what is the manager email?"
     },
     {
-        type:"input"
-        name:"office"
-        message:"what is manager office number??"
+        type:"input",
+        name:"office",
+        message:"what is the manager office number??"
     }
-    }).then(ans => {
+    ]).then(ans => {
         const newManager = new Manager(ans.name, ans.id, ans.email, ans.office)
-        employed.push(newManager);
+        employed.push(newManager); 
     })
+    todo();
 }
 
 const addEngineer = () => {
-    inquirer.prompt({
-       {type:"input"
-        name:"name"
+    inquirer.prompt([
+    {   
+        type:"input",
+        name:"name",
         message:"What Engineer name?"
     },
     {
-        type:"input"
-        name:"id"
+        type:"input",
+        name:"id",
         message:"what is Engineer id?"
     },
     {
-        type:"input"
-        name:"email"
+        type:"input",
+        name:"email",
         message:"what is Engineer email?"
     },
     {
-        type:"input"
-        name:"github"
+        type:"input",
+        name:"github",
         message:"what is Engineer Github?"
     }
-    }).then(ans => {
+    ]).then(ans => {
         const newEngineer = new Engineer(ans.name, ans.id, ans.email, ans.github)
         employed.push(newEngineer);
     })
+    todo();
 }
+
+addManager();
